27643bede0858b79ee5c5420f5cee398d1c525ac
Justin Logan <juslog24@gmail.com>
2025-07-07 00:40:15 -0700

    Defined weekData & added prediction

---
+++ myDataJournal.js
@@ -0,0 +1,18 @@
+let weekData = [
+    {day : 'Sunday', sleepHours : {}, screenTime : {}, mood : {}, caffeineIntake : {}, focusLevel : {}},
+    {day : 'Monday', sleepHours : {}, screenTime : {}, mood : {}, caffeineIntake : {}, focusLevel : {}},
+    {day : 'Tuesday', sleepHours : {}, screenTime : {}, mood : {}, caffeineIntake : {}, focusLevel : {}},
+    {day : 'Wednesday', sleepHours : {}, screenTime : {}, mood : {}, caffeineIntake : {}, focusLevel : {}},
+    {day : 'Thursday', sleepHours : {}, screenTime : {}, mood : {}, caffeineIntake : {}, focusLevel : {}},
+    {day : 'Friday', sleepHours : {}, screenTime : {}, mood : {}, caffeineIntake : {}, focusLevel : {}},
+    {day : 'Saturday', sleepHours : {}, screenTime : {}, mood : {}, caffeineIntake : {}, focusLevel : {}},
+]
+
+// Which day had the most screen time?
+// Friday had the most screen time.
+// 
+// Best focus day?
+// Monday was the best focus day.
+//
+// Is more caffeine helping?
+// No, more caffeine is not helping/
